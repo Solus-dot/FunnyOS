5ec5e3e54c55597e9b7525739bdf8c26649310e8
Solus-dot <solus23451@gmail.com>
2025-03-31 18:51:40 +0530

    Added Floppy disk reading and bochs support.

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -3,8 +3,10 @@
 
 %define ENDL 0x0D, 0x0A
 
-
+;
 ; FAT 12 HEADERS
+;
+
 jmp short start
 nop
 
@@ -24,9 +26,9 @@ bdb_large_sector_count:     dd 0
 
 ; Extended Boot Record
 ebr_drive_number:           db 0                        ; 0x00 floppy, 0x80 hdd
-                            db 0                        ; reserved byte
+                            db 0                        ; Reserved byte
 ebr_signature:              db 0x29
-ebr_volume_id:              db 11h, 69h, 42h, 13h       ; serial number, values don't matter
+ebr_volume_id:              db 0x11, 0x69, 0x42, 0x13   ; Serial number, values don't matter
 ebr_volume_label:           db 'FunnyOS    '            ; 11 bytes, padded with spaces
 ebr_system_id:              db 'FAT12   '               ; 8 bytes, padded with spaces
 
@@ -38,46 +40,168 @@ start:
 ; Params:
 ;   - ds:si points to string
 puts:
-    ; save registers that are to be modified
+    ; Save registers that are to be modified
     push si
     push ax
 
 .loop:
     lodsb               ; Loads next character in al
     or al, al           ; verify if next character is null (al == 0)
-    jz .done            ; if null character, finish the function
+    jz .done            ; If null character, finish the function
 
     mov ah, 0x0E        ; Call BIOS Interrupt
     mov bh, 0
     int 0x10
-    jmp .loop           ; loop back until al == 0 [string has been fully printed]
+    jmp .loop           ; Loop back until al == 0 [string has been fully printed]
 
 .done:
-    ; remove saved registers after function is done executing
+    ; Remove saved registers after function is done executing
     pop ax
     pop si
     ret
 
 
 main:
-    ; setup data segments
+    ; Setup data segments
     mov ax, 0           ; Cannot write to ds/es directly\
     mov ds, ax
     mov es, ax
 
-    ; setup stack
+    ; Setup stack
     mov ss, ax
     mov sp, 0x7C00      ; stack grows downwards from where we loaded our memory
 
+    ; Read something from the floppy disk
+    ; BIOS should set DL to drive number
+    mov [ebr_drive_number], dl
+
+    mov ax, 1           ; LBA=1, second sector from disk
+    mov cl, 1           ; 1 sector to read
+    mov bx, 0x7E00      ; Data should be after the bootloader
+    call disk_read
+
+    ; Print message1
     mov si, message1
     call puts
 
+    cli                 ; Disables interrupts so that CPU does not get out of halt state
     hlt                 ; Stops CPU from executing
 
-.halt:
-    jmp .halt           ; Infinite Loop
+;
+; ERROR HANDLERS
+;
 
-message1: db 'Hello Guys, FunnyOS Here!', ENDL, 0
+floppy_error:
+    mov si, msg_read_failed
+    call puts
+    jmp wait_key_and_reboot
+
+wait_key_and_reboot:
+    mov ah, 0
+    int 0x16            ; Wait for keypress
+    jmp 0FFFFh:0        ; Jump to beginning to BIOS, should reboot
+
+.halt:
+    cli                 ; Disables interrupts so that CPU does not get out of halt state
+    hlt                 
+
+;
+; DISK ROUTINES
+;
+
+; Converts an LBA (Logical Block Addressing) address to CHS (Cylinder, Head, Sector) Address
+; Parameters:
+;   - ax: LBA Address
+; Returns:
+;   - cx [bits 0-5]: sector number
+;   - cx [bits 6-15]: cylinder
+;   - dh: head
+lba_to_chs:
+    push ax
+    push dx
+
+    xor dx, dx                          ; dx = 0
+    div word [bdb_sectors_per_track]    ; ax = LBA / SectorsPerTrack
+                                        ; dx = LBA % SectorsPerTrack
+    inc dx                              ; dx = (LBA % SectorsPerTrack) + 1 = sector
+    mov cx, dx                          ; cx = sector
+
+    xor dx, dx                          ; dx = 0
+    div word [bdb_heads]                ; ax = (LBA / SectorsPerTrack) / Heads = cylinder
+                                        ; dx = (LBA / SectorsPerTrack) % Heads = head
+    mov dh, dl                          ; dh = head
+    mov ch, al                          ; ch = cylinder (lower 8 bits)
+    shl ah, 6                           ; shl left shifts bits (<< in C)
+    or cl, ah                           ; Put upper 2 bits of cylinder in CL
+
+    pop ax
+    mov dl, al                          ; Restore DL
+    pop ax
+    ret
+
+; Reads sectors from a disk
+; Parameters:
+;   - ax: LBA address
+;   - cl: Number of sectors to read (upto 128)
+;   - dl: Drive number
+;   - es:bx: Memory address to store read data
+disk_read:
+    push ax                             ; Save registers to be modified
+    push bx
+    push cx
+    push dx
+    push di
+
+    push cx                             ; Temporarily save CL (Number of sectors to read)
+    call lba_to_chs                     ; Compute CHS
+    pop ax                              ; AL = number of sectors to read
+
+    mov ah, 0x02
+    mov di, 3                           ; retry count
+
+.retry:
+    pusha                               ; Save all registers, we do not know what BIOS Modifies
+    stc                                 ; Set carry flag, Some BIOS'es do not set it
+    int 0x13                            ; Carry flag cleared = success
+    jnc .done                           ; Jump if carry not set
+
+    ; Read failed
+    popa
+    call disk_reset
+    dec di
+    test di, di
+    jnz .retry
+
+.fail:
+    ; All attempts are exhausted
+    jmp floppy_error
+
+.done:
+    popa
+
+    pop di                             ; Save modified registers
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+
+; Resets disk controller
+; Parameters:
+;   - dl: Drive number
+disk_reset:
+    pusha
+    mov ah, 0
+    stc
+    int 0x13
+    jc floppy_error
+    popa
+    ret
+
+
+
+message1:               db 'Hello Guys, FunnyOS Here!', ENDL, 0
+msg_read_failed:        db 'Read from disk failed!', ENDL, 0
 
 times 510-($-$$) db 0   ; Fill remaining space with zeros (510 bytes + the boot signature bytes 0x55 and 0xAA, ensuring 512 bytes in boot sector)
 dw 0xAA55               ; boot signature
