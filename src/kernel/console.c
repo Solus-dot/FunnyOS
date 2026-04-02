619dc3c03d6302d5ef0609c8afccf8ca52468edd
Solus-dot <solus23451@gmail.com>
2026-04-03 01:11:00 +0530

    Update VGA hardware cursor position

--- src/kernel/console.c
+++ src/kernel/console.c
@@ -1,10 +1,15 @@
 #include "console.h"
+#include "io.h"
 #include "kstring.h"
 #include "serial.h"
 
 #define VGA_WIDTH 80u
 #define VGA_HEIGHT 25u
 #define VGA_ATTRIBUTE 0x07u
+#define VGA_CRTC_INDEX_PORT 0x3D4u
+#define VGA_CRTC_DATA_PORT 0x3D5u
+#define VGA_CURSOR_HIGH_INDEX 14u
+#define VGA_CURSOR_LOW_INDEX 15u
 
 static volatile uint16_t* const g_vga = (volatile uint16_t*)0xB8000;
 static uint16_t g_row = 0;
@@ -13,6 +18,16 @@ static uint16_t g_col = 0;
 static void put_vga_at(uint16_t row, uint16_t col, char c)
 {
     g_vga[row * VGA_WIDTH + col] = (uint16_t)VGA_ATTRIBUTE << 8 | (uint8_t)c;
+}
+
+static void update_cursor(void)
+{
+    uint16_t position = (uint16_t)(g_row * VGA_WIDTH + g_col);
+
+    io_out8(VGA_CRTC_INDEX_PORT, VGA_CURSOR_HIGH_INDEX);
+    io_out8(VGA_CRTC_DATA_PORT, (uint8_t)(position >> 8));
+    io_out8(VGA_CRTC_INDEX_PORT, VGA_CURSOR_LOW_INDEX);
+    io_out8(VGA_CRTC_DATA_PORT, (uint8_t)(position & 0xFFu));
 }
 
 static void scroll_if_needed(void)
@@ -32,6 +47,7 @@ static void scroll_if_needed(void)
         put_vga_at((uint16_t)(VGA_HEIGHT - 1), col, ' ');
 
     g_row = (uint16_t)(VGA_HEIGHT - 1);
+    update_cursor();
 }
 
 void console_init(void)
@@ -45,6 +61,7 @@ void console_write_char(char c)
     if (c == '\r') {
         g_col = 0;
         serial_write_byte('\r');
+        update_cursor();
         return;
     }
 
@@ -54,6 +71,7 @@ void console_write_char(char c)
         scroll_if_needed();
         serial_write_byte('\r');
         serial_write_byte('\n');
+        update_cursor();
         return;
     }
 
@@ -66,6 +84,8 @@ void console_write_char(char c)
         ++g_row;
         scroll_if_needed();
     }
+
+    update_cursor();
 }
 
 void console_write_n(const char* s, size_t count)
@@ -99,6 +119,7 @@ void console_clear(void)
 
     g_row = 0;
     g_col = 0;
+    update_cursor();
 }
 
 void console_backspace(void)
@@ -115,4 +136,5 @@ void console_backspace(void)
 
     put_vga_at(g_row, g_col, ' ');
     serial_write("\b \b");
+    update_cursor();
 }
